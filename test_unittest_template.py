372c66d911dcd1ade2390281352f23ff937c33aa
tkc <xxtkcxx@gmail.com>
2019-01-09 23:56:00 +0900

    first commit

---
+++ test_unittest_template.py
@@ -0,0 +1,21 @@
+import unittest
+
+
+class TestStringMethods(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # 個別のクラス内のテストが実行される前に呼び出される
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        # 個別のクラス内のテストが実行された後に呼び出される
+        pass
+
+    def setUp(self):
+        # テストメソッドの直前に呼び出される
+        pass
+
+    def tearDown(self):
+        # テストメソッドが実行され、結果が記録された直後に呼び出される
+        pass
